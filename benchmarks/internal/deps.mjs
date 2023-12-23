2cef34c6398894fa69fff5aed7a4d32976b654c0
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-12-23 21:05:32 +0100

    chore: bump deno standard library version
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- benchmarks/internal/deps.mjs
+++ benchmarks/internal/deps.mjs
@@ -1,1 +1,1 @@
-export { parseArgs } from 'https://deno.land/std@0.209.0/cli/parse_args.ts'
+export { parseArgs } from 'https://deno.land/std@0.210.0/cli/parse_args.ts'
--- tests/deps.mjs
+++ tests/deps.mjs
@@ -2,4 +2,4 @@ export {
   assertSpyCalls,
   returnsNext,
   stub,
-} from 'https://deno.land/std@0.209.0/testing/mock.ts'
+} from 'https://deno.land/std@0.210.0/testing/mock.ts'
