6a3486fc3285182853d434be74dece19c570e9ae
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-12-24 23:01:51 +0100

    build: silence linter
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tests/pools/abstract-pool.test.mjs
+++ tests/pools/abstract-pool.test.mjs
@@ -1,4 +1,4 @@
-import { assertSpyCalls, returnsNext, stub } from '$std/testing/mock.ts'
+import { /* assertSpyCalls, */ returnsNext, stub } from '$std/testing/mock.ts'
 import { EventEmitter } from 'node:events'
 import { expect } from 'expect'
 import {
