d58be199881d31517b7c5f1e5eabd3c51c6986ac
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-08-14 19:34:31 +0200

    chore(master): release 0.4.18

--- examples/deno/javascript/fixedExample.js
+++ examples/deno/javascript/fixedExample.js
@@ -2,7 +2,7 @@ import {
   availableParallelism,
   FixedThreadPool,
   PoolEvents,
-} from 'jsr:@poolifier/poolifier-web-worker@^0.4.17' // x-release-please-version
+} from 'jsr:@poolifier/poolifier-web-worker@^0.4.18' // x-release-please-version
 
 const pool = new FixedThreadPool(
   availableParallelism(),
