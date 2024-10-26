e94a1e3f287b75d214f5c6364309355ebb6aaa4a
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-10-26 22:07:52 +0200

    chore(master): release 0.4.31 (#72)
    * chore(master): release 0.4.31
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- examples/deno/javascript/fixedExample.js
+++ examples/deno/javascript/fixedExample.js
@@ -2,7 +2,7 @@ import {
   availableParallelism,
   FixedThreadPool,
   PoolEvents,
-} from 'jsr:@poolifier/poolifier-web-worker@^0.4.30' // x-release-please-version
+} from 'jsr:@poolifier/poolifier-web-worker@^0.4.31' // x-release-please-version
 
 const pool = new FixedThreadPool(
   availableParallelism(),
--- src/pools/version.ts
+++ src/pools/version.ts
@@ -1,3 +1,3 @@
 // x-release-please-start-version
-export const version = '0.4.30'
+export const version = '0.4.31'
 // x-release-please-end
