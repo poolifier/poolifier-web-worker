1e5285fd0a7fbaa0e4282c84e9bb2571853ab9c1
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-07-02 16:07:12 +0200

    chore(master): release 0.5.0

--- examples/deno/javascript/yourWorker.js
+++ examples/deno/javascript/yourWorker.js
@@ -1,4 +1,4 @@
-import { ThreadWorker } from 'jsr:@poolifier/poolifier-web-worker@^0.4.31' // x-release-please-version
+import { ThreadWorker } from 'jsr:@poolifier/poolifier-web-worker@^0.5.0' // x-release-please-version
 
 function yourFunction() {
   for (let i = 0; i <= 1000; i++) {
