3483e16d3346aa8240d286f80ad594f2b3667288
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-07-11 12:21:25 +0200

    chore(master): release 0.5.6 (#89)
    * chore(master): release 0.5.6
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- examples/deno/javascript/yourWorker.js
+++ examples/deno/javascript/yourWorker.js
@@ -1,4 +1,4 @@
-import { ThreadWorker } from 'jsr:@poolifier/poolifier-web-worker@^0.5.5' // x-release-please-version
+import { ThreadWorker } from 'jsr:@poolifier/poolifier-web-worker@^0.5.6' // x-release-please-version
 
 function yourFunction() {
   for (let i = 0; i <= 1000; i++) {
