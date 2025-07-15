4cae20a7e1f0a2483459767c6d77c082648016a2
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-07-15 19:41:36 +0200

    chore(master): release 0.5.8

--- examples/deno/typescript/pool.ts
+++ examples/deno/typescript/pool.ts
@@ -2,7 +2,7 @@ import {
   availableParallelism,
   DynamicThreadPool,
   FixedThreadPool,
-} from 'jsr:@poolifier/poolifier-web-worker@^0.5.7' // x-release-please-version
+} from 'jsr:@poolifier/poolifier-web-worker@^0.5.8' // x-release-please-version
 import type { MyData, MyResponse } from './worker.ts'
 
 const workerFileURL = new URL('./worker.ts', import.meta.url)
